57888c6af502094237d131deea04e2da69ae3162
rev_e <rev_skivl@hotmail.com>
2022-09-28 23:38:10 -0300

    Primera Versión

--- app/src/main/java/com/example/calculadorabasica/MainActivity.java
+++ app/src/main/java/com/example/calculadorabasica/MainActivity.java
@@ -132,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
             }
             div.putExtra("operacion", divi.getText().toString());
             startActivity(div);
+
         }
     }
 
